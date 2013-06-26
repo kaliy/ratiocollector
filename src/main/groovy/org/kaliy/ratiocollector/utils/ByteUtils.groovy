dd82f13075122789aa437006bac22e288a83e426
Arthur Kalimullin <kalimullin@gmail.com>
2013-06-26 15:20:50 +0600

    ByteUtils - string converter completed

--- src/main/groovy/org/kaliy/ratiocollector/utils/ByteUtils.groovy
+++ src/main/groovy/org/kaliy/ratiocollector/utils/ByteUtils.groovy
@@ -5,8 +5,15 @@ class ByteUtils {
             "(?i)" + // Case insensitive
             "([0-9]+(?:\\.[0-9]*)?)" + // Integer or decimal number
             "\\s*" + // Any whitespace character before...
-            "([KMPT]?)" // Unit
+            "([KMGTP]?)" // Unit
     static Long convertStringToBytes(String rawBytesString) {
-        null
+        def numberMatcher = rawBytesString=~UNITS_REGEXP
+        def units = ["", "K", "M", "G", "T", "P"]
+        if (0 == numberMatcher.size()) {
+            throw new IllegalArgumentException("Invalid size string")
+        }
+        String size = numberMatcher[0][1]
+        String unit = numberMatcher[0][2]
+        return size.toDouble()*(1024**(units.indexOf(unit.toUpperCase()))).toLong()
     }
 }
--- src/test/groovy/org/kaliy/ratiocollector/utils/ByteUtilsTest.groovy
+++ src/test/groovy/org/kaliy/ratiocollector/utils/ByteUtilsTest.groovy
@@ -13,22 +13,22 @@ class ByteUtilsTest extends GroovyTestCase {
 
     void testStringConverterInterpretsMegabytesWithSpace() {
         def bytes = "20 Mb"
-        assert 1024**2 * 20 == ByteUtils.convertStringToBytes(bytes)
+        assert 1024L**2 * 20 == ByteUtils.convertStringToBytes(bytes)
     }
 
     void testStringConverterInterpretsGigabytesWithSpace() {
         def bytes = "20 Gb"
-        assert 1024**3 * 20 == ByteUtils.convertStringToBytes(bytes)
+        assert 1024L**3 * 20 == ByteUtils.convertStringToBytes(bytes)
     }
 
     void testStringConverterInterpretsTerabytesWithSpace() {
         def bytes = "20 Tb"
-        assert 1024**4 * 20 == ByteUtils.convertStringToBytes(bytes)
+        assert 1024L**4 * 20 == ByteUtils.convertStringToBytes(bytes)
     }
 
     void testStringConverterInterpretsPetabytesWithSpace() {
         def bytes = "20 Pb"
-        assert 1024**5 * 20 == ByteUtils.convertStringToBytes(bytes)
+        assert 1024L**5 * 20 == ByteUtils.convertStringToBytes(bytes)
     }
 
     void testStringConverterInterpretsOneCharacterUnit() {
@@ -44,5 +44,22 @@ class ByteUtilsTest extends GroovyTestCase {
     void testStringConverterInterpretsIecBinaryPrefix() {
         def bytes = "20 KiB"
         assert 1024 * 20 == ByteUtils.convertStringToBytes(bytes)
+    }
+
+    void testStringConverterInterpretsDecimalNumbers() {
+        def bytes = "20.25 K"
+        assert 1024 * 20.25 == ByteUtils.convertStringToBytes(bytes)
+    }
+
+    void testStringConverterInterpretsDecimalNumbersWithWrongConversion() {
+        def bytes = "20.259 K"
+        assert (1024 * 20.259).toLong() == ByteUtils.convertStringToBytes(bytes)
+    }
+
+    void testStringConverterThrowsExceptionIfStringDoesntContainSize() {
+        def bytes = "Never gonna give you up"
+        shouldFail(IllegalArgumentException) {
+            ByteUtils.convertStringToBytes(bytes)
+        }
     }
 }
